2e5db8b4c0154a72a48fbb1b9151b7fdc06408b9
Thiannight <thianntatiana@gmail.com>
2026-03-31 06:30:22 +0300

    fix : datasource issue

--- src/main/java/hei/school/ingredient/datasource/DataSourceConfig.java
+++ src/main/java/hei/school/ingredient/datasource/DataSourceConfig.java
@@ -3,12 +3,27 @@ package hei.school.ingredient.datasource;
 import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class DataSourceConfig {
 
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
     @Bean
     public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+        return DataSourceBuilder.create()
+                .driverClassName("org.postgresql.Driver")
+                .url(url)
+                .username(username)
+                .password(password)
+                .build();
     }
 }
